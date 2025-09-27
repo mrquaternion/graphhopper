164dd0e540c6f38c9bd45e7b320dab0a39630e66
marcelo <marcelo.david.amarilla-hernandez@umontreal.ca>
2025-09-27 13:21:07 -0400

    Tests de la classe com.graphhopper.config.Profile.java

---
+++ core/src/test/java/com/graphhopper/config/ProfileTest.java
@@ -0,0 +1,43 @@
+package com.graphhopper.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProfileTest {
+    Profile instance = new Profile("name");
+
+    @Test
+    public void testValidateProfileName(){
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Profile.validateProfileName("#not_ok?");
+        });
+
+        assertDoesNotThrow(()-> {Profile.validateProfileName("ok_name");});
+    }
+
+    @Test
+    public void testPutHint(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            instance.putHint("u_turn_costs", "whatever");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            instance.putHint("vehicle", "whatever");
+        });
+    }
+
+    @Test
+    public void testEquals(){
+        String otherClass = "not null, just another class";
+        String nullObject = null;
+        Profile sameRef = instance;
+        Object sameName = new Profile("name");
+
+        assertNotEquals(instance, otherClass);
+        assertNotEquals(instance, nullObject);
+        assertEquals(instance,sameRef);
+        assertEquals(instance,sameName);
+    }
+}
